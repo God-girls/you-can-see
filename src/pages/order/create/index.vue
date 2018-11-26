de05f9efa05dde375b1f4a83bb091d97e4e8e8d8
xiaoxiao <448759162@qq.com>
2018-11-26 16:33:45 +0800

    upload

--- src/pages/order/create/index.vue
+++ src/pages/order/create/index.vue
@@ -133,15 +133,16 @@ export default {
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
-              alert(JSON.stringify(res))
+              // alert(JSON.stringify(res))
               var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+              this.imgUrl = localIds
               _this.uploadImg(localIds)
           }
       });      
 
     },
     uploadImg(localIds){
-      alert(localIds)
+      alert('uploadImage1')
       wx.uploadImage({
         localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
         isShowProgressTips: 1,// 默认为1，显示进度提示
